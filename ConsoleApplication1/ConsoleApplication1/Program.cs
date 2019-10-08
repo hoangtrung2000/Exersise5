3a167dc2dc228c92973588e9b3de386db9e898a7
HoangTrung <hoangtrung7762@gmail.com>
2019-10-08 17:54:37 +0700

    Exersise5

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,87 @@ namespace Exersise5A
     {
         static void Main(string[] args)
         {
+          //  Print_a_2_z_v1();
+           // Print_z_2_a_v1();
+            //Print_a_2_z_v2();
+            //Print_a_2_z_v3();
+            //Print_A_2_Z();
+            Console.WriteLine(IsLower('A'));
+                //Char2Code
+            //while (true)
+            //{
+            //    char c = Console.ReadKey().KeyChar;
+            //    Console.WriteLine(" " + Char2Code(c));
+
+            //}
 
         }
+        public static void Print_a_2_z_v1()
+        {
+            int n = 0x61;
+            for (int i = 1; i <= 26; i++)
+            {             
+                Console.WriteLine((char)n);
+                n++;
+            }
+        }
+        public static void Print_a_2_z_v2()
+        {
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                Console.WriteLine(c);
+            }
+        }
+        public static void Print_a_2_z_v3()
+        {
+            //string s = "abcdeefghijlkmnoqrstuvwxyz";
+            //for (int i = 0; i < s.Length; i++)
+            //{
+            //    Console.WriteLine(s[i]);
+            //}
+           
+        }
+        public static void Print_z_2_a_v1()
+        {
+            int n = 0x7A;
+            for (int i = 1; i <= 26; i++)
+            {
+                Console.WriteLine((char)n);
+                n--;
+            }
+        }
+        public static void Print_A_2_Z()
+        {
+            int n = 0x41;
+            for(int i = 1; i <= 26; i++ )
+            {
+                Console.WriteLine((char)n);
+                n++;
+            }
+        }
+        public static int Char2Code(char c)
+        {
+            return (int)c;
+        }
+        public static bool IsUpper(char c)
+        {
+            return false;
+        }
+        public static bool IsLower(char c)
+        {
+            string s = "abcdefghijlkmnoqrstuvwxyz";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (c == s[i])
+                    return true;
+            }
+            return false;
+        }
+        public static char ToUpper(char c)
+        {
+            return c;
+        }
+
+
     }
 }
